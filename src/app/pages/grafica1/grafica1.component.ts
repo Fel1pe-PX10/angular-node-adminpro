af8359e3f50f05b2a69eadffa287ca9831c3d386
Felipe Paez <ofpaez87@gmail.com>
2022-08-24 16:16:02 -0500

    Componente de gráficas

--- src/app/pages/grafica1/grafica1.component.ts
+++ src/app/pages/grafica1/grafica1.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-grafica1',
   templateUrl: './grafica1.component.html',
@@ -7,6 +8,11 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class Grafica1Component implements OnInit {
+
+  labels1: string[] = ['Download Sales1', 'In-Store Sales1', 'Mail-Order Sales1'];
+  data1 = [
+    [455, 200, 10]
+  ];
 
   constructor() { }
 
