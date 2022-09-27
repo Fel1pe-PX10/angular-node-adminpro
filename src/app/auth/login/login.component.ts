192585f23f35cfd4b1430c1a4679498cb8c5baec
Felipe Paez <ofpaez87@gmail.com>
2022-09-26 20:19:20 -0500

    Login conectando backend - Parte 1

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,20 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+import { UsuarioService } from '../../services/usuario.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  
+  public loginForm = this.fb.group({
+    email:    [localStorage.getItem('email') || '', [Validators.required, Validators.email]],
+    password: ['123456', [Validators.required]],
+    remember: [false]
+  });
+  
+  constructor(private fb: FormBuilder,
+              private router:Router,
+              private usuarioService: UsuarioService ) { }
 
-  constructor( private router:Router ) { }
-
-  ngOnInit(): void {
-  }
+  
 
   login(){
-    this.router.navigateByUrl('/');
+    console.log(this.loginForm.value);
+    this.usuarioService.login(this.loginForm.value)
+      .subscribe(resp => {
+        console.log(resp);
+
+        if(this.loginForm.get('remember')?.value){
+          localStorage.setItem('email', this.loginForm.get('email')?.value)
+        }
+        else{
+          localStorage.removeItem('email');
+        }
+      }, (err) => {
+        Swal.fire('Error', err.error.msg, 'error');
+      });
+    //this.router.navigateByUrl('/');
   }
 
 }
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,15 +1,90 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import Swal from 'sweetalert2'
+
+import { UsuarioService } from '../../services/usuario.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
 
-  constructor() { }
+  public formSubmitted = false;
 
-  ngOnInit(): void {
+  public registerForm = this.fb.group({
+    nombre:   ['Felipe', [Validators.required, Validators.minLength(3)]],
+    email:    ['test100@gmail.com', [Validators.required, Validators.email]],
+    password: ['123456', [Validators.required]],
+    password2:['123456', [Validators.required]],
+    terminos: [ true , [Validators.required]]
+  },
+  {
+    validators: this.passwordIguales('password', 'password2')
+  });
+  
+  constructor(private fb: FormBuilder,
+              private usuarioService: UsuarioService) { }
+
+  crearUsuario(){
+    this.formSubmitted = true;
+    console.log(this.registerForm.value);
+    //console.log(this.registerForm);
+
+    if(this.registerForm.invalid){
+      return;
+    }
+
+    this.usuarioService.crearUsuario(this.registerForm.value)
+      .subscribe(resp => {
+        console.log('usuario Creado');
+        console.log(resp);
+      }, (err) => {
+        Swal.fire('Error', err.error.msg, 'error');
+      });
+  }
+  
+  campoNoValido( campo: string): boolean{
+    if(this.registerForm.get(campo)?.invalid && this.formSubmitted){
+      return true;
+    }
+    else{
+      return false;
+    }
+      
+  }
+
+  contrasenasNoValidas(){
+    const pass1 = this.registerForm.get('password')?.value;
+    const pass2 = this.registerForm.get('password2')?.value;
+
+    if((pass1 !== pass2) && this.formSubmitted){
+      return true
+    }
+    else
+    return false
+  }
+
+  aceptaTerminos(): boolean {
+    return !this.registerForm.get('terminos')?.value && this.formSubmitted;
+  }
+
+  passwordIguales(pass: string, pass2: string){
+    
+    return (formGroup: FormGroup) => {
+      
+      const pass1Control = formGroup.get(pass);
+      const pass2Control = formGroup.get(pass2);
+
+      if(pass1Control?.value == pass2Control?.value){
+        pass2Control?.setErrors(null);
+      }
+      else{
+        pass2Control?.setErrors({noEsIgual: true});
+      }
+    }
   }
 
 }
